def004e2190c63634398b13c8940de5521bc54db
zattaoui <03zaki95@gmail.com>
2022-01-02 14:13:18 +0100

    commit 02/01/2022 for use mongodb

--- src/coffees/entities/coffee.entitie.ts
+++ src/coffees/entities/coffee.entitie.ts
@@ -1,6 +1,25 @@
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Flavor } from './flavor.entity';
+
+// sql table === 'coffee'
+@Entity()
 export class Coffee {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   brand: string;
+
+  @JoinTable()
+  @ManyToMany((type) => Flavor, (flavor) => flavor.coffees)
   flavors: string[];
 }
